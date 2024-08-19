3a7cf1f13938f5fed8e8b524a654ce7a448794fe
Zoé Moeremans <zoemoeremans@zoes-air.home>
2024-08-19 14:02:56 +0200

    homepage in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/login_view.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:notes_app/firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +18,41 @@ class NotesApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginView(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              // if firebase is initialized successfully
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('You are a verified user.');
+              } else {
+                print('You need to verify your email first.');
+              }
+              return const Text('Done');
+            default:
+              // while firebase is still initializing, show a loading message
+              return const Text('Loading...');
+          }
+        },
+      ),
     );
   }
 }
