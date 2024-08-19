c77f80bb85254b65ec5284f6c6585ea83220aa71
Zoé Moeremans <zoemoeremans@zoes-air.home>
2024-08-19 13:45:46 +0200

    error handling during registration

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -66,9 +66,22 @@ class _RegisterViewState extends State<RegisterView> {
                       final email = _email.text;
                       final password = _password.text;
 
-                      final userCredential = await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                              email: email, password: password);
+                      try {
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        print(userCredential);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          print('error during registration : Weak password');
+                        } else if (e.code == 'email-already-in-use') {
+                          print('error during registration : Email is already in use');
+                        } else if (e.code == 'invalid-email') {
+                          print('error during registration : Invalid email entered');
+                        } else {
+                          print('Error during registration : ${e.code}');
+                        }
+                      }
                     },
                     child: const Text('Register'),
                   ),
