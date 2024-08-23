4483eaa03cca3e932d0fbb6f85f7ba2c99bc0e26
Zoé Moeremans <zoemoeremans@Zoes-MacBook-Air.local>
2024-08-23 13:13:06 +0200

    implemented authentification provider

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,14 @@
+import 'package:notes_app/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+
+  Future<AuthUser> createUser(
+      {required String email, required String password});
+
+  Future<void> sendEmailVerification();
+
+  Future<AuthUser> logIn({required String email, required String password});
+
+  Future<void> logOut();
+}
