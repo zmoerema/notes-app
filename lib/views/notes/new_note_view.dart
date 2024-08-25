063c5a3c323fff425f7405407759208e465945d7
Zoé Moeremans <zoemoeremans@Zoes-MacBook-Air.local>
2024-08-25 21:52:16 +0200

    implemented logic behind new note view

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/models/note.dart';
+import 'package:notes_app/services/auth/auth_service.dart';
+import 'package:notes_app/services/db/db_service.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({super.key});
@@ -8,6 +11,60 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+  DatabaseNote? _note;
+  late final DbService _db;
+  late final TextEditingController _textController;
+
+  @override
+  void initState() {
+    _db = DbService();
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  void _textControllerListener() async {
+    // listens to changes in the text controller
+    // updates the note in the database
+    if (_note == null) return;
+    
+    await _db.updateNote(note: _note!, text: _textController.text);
+  }
+
+  void _setUpTextControllerListener() async {
+    _textController.removeListener(_textControllerListener); // to avoid adding multiple listeners, which could lead to unwanted behavior
+    _textController.addListener(_textControllerListener); // add _textControllerListener as the listener to the _textController
+  }
+
+  void _deleteNoteIfTextIsEmpty() {
+    if (_textController.text.isEmpty && _note != null) {
+      _db.deleteNote(noteId: _note!.id);
+    }
+  }
+
+  void _saveNoteIfTextIsNotEmpty() async {
+    if (_note != null && _textController.text.isNotEmpty) {
+      _db.updateNote(note: _note!, text: _textController.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextIsNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  Future<DatabaseNote> createNewNote() async {
+    if (_note != null) {
+      return _note!;
+    }
+
+    final currentUser = AuthService.firebase().currentUser!;
+    final noteOwner = await _db.getUser(email: currentUser.email!);
+    return await _db.createNote(owner: noteOwner);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
