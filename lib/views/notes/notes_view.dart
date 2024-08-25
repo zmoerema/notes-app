1200e504ff2fb837dca22338fdd3f8a11ba10649
Zoé Moeremans <zoemoeremans@Zoes-MacBook-Air.local>
2024-08-25 20:43:48 +0200

    created notes folder in views folder + created new note view

---
+++ lib/views/notes/notes_view.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/services/auth/auth_service.dart';
+import 'package:notes_app/services/db/db_service.dart';
+import 'package:notes_app/utilities/constants/routes.dart';
+import 'package:notes_app/utilities/constants/menu_action.dart';
+import 'dart:developer' as devtools show log;
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  late final DbService _dbService;
+  String get userEmail => AuthService.firebase().currentUser!.email!;
+
+  @override
+  void initState() {
+    _dbService = DbService();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _dbService.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Notes'),
+        actions: [
+          PopupMenuButton<MenuAction>(onSelected: (value) async {
+            if (value case MenuAction.logout) {
+              final shouldLogout = await showLogOutDialog(context);
+              if (shouldLogout) {
+                await AuthService.firebase().logOut();
+                devtools.log('log out');
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil(LOGIN_ROUTE, (_) => false);
+              }
+            }
+          }, itemBuilder: (context) {
+            return [
+              const PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,
+                child: Text('Log out'),
+              ),
+            ];
+          }),
+        ],
+      ),
+      body: FutureBuilder(
+          future: _dbService.getOrCreateUser(email: userEmail),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return StreamBuilder(
+                    stream: _dbService.allNotes,
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.done:
+                          return const Text('done');
+                        case ConnectionState.waiting:
+                          return const Text('loading...');
+                        default:
+                          return const CircularProgressIndicator();
+                      }
+                    });
+              default:
+                return const CircularProgressIndicator();
+            }
+          }),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Sign out'),
+          content: const Text('Are you sure you want to sign out ?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Log out')),
+          ],
+        );
+      }).then((value) => value ?? false);
+}
